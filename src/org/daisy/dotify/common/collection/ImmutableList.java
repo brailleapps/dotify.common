c69787f5272fa2addb4be3f43509ce92e3c9fa72
Bert Frees <bertfrees@gmail.com>
2018-09-19 15:09:26 +0200

    Add javadocs to PersistentList and PersistentMap

--- src/org/daisy/dotify/common/collection/ImmutableList.java
+++ src/org/daisy/dotify/common/collection/ImmutableList.java
@@ -2,10 +2,25 @@ package org.daisy.dotify.common.collection;
 
 import java.util.List;
 
+/**
+ * An immutable list, i.e. a list of which the content is guaranteed not to change.
+ *
+ * All the "destructive" methods ({@link List#set}, {@link java.util.Collection#add}, {@link
+ * java.util.Collection#remove}, etc.) in this class will throw an UnsupportedOperationException.
+ *
+ * Instances of this class that are derived from each other are implemented via a common persistent
+ * data structure, i.e. a data structure that preserves previous versions of itself.
+ */
 public class ImmutableList<E> extends PersistentList.View<E> implements List<E> {
 
+	@SuppressWarnings("rawtypes")
+	private static final ImmutableList EMPTY = new ImmutableList<>();
+
+	/**
+	 * Get an empty instance of ImmutableList.
+	 */
 	public static <E> ImmutableList<E> empty() {
-		return new ImmutableList<E>();
+		return (ImmutableList<E>)EMPTY;
 	}
 
 	private ImmutableList() {
@@ -20,10 +35,18 @@ public class ImmutableList<E> extends PersistentList.View<E> implements List<E> 
 		super(from, true);
 	}
 
+	/**
+	 * Get a ImmutableList.Builder with the same initial contents as this list, in other words, a
+	 * mutable version of this list.
+	 */
 	public Builder<E> builder() {
 		return new Builder<E>(this);
 	}
 
+	/**
+	 * A builder of an ImmutableList. Does not support removing elements, or adding elements at
+	 * other positions than at the end of the list.
+	 */
 	// not extending ImmutableList so that Builder can not be accidentally passed when immutable object is expected
 	public static class Builder<E> extends PersistentList.View<E> implements List<E>, Cloneable {
 
@@ -31,6 +54,9 @@ public class ImmutableList<E> extends PersistentList.View<E> implements List<E> 
 			super(from, false);
 		}
 
+		/**
+		 * Build an immutable version of this list.
+		 */
 		public ImmutableList<E> build() {
 			return new ImmutableList<E>(this);
 		}
@@ -52,6 +78,11 @@ public class ImmutableList<E> extends PersistentList.View<E> implements List<E> 
 		readonly = true;
 	}
 
+	/**
+	 * Non-destructive version of {@link java.util.Collection#add}. Instead of returning a boolean
+	 * value (which is always true), this method will return a new List object with the given
+	 * element added.
+	 */
 	public static <E> ImmutableList<E> add(ImmutableList<E> list, E e) {
 		// return list.builder().add(e).build();
 		return new ImmutableList<E>(list, e);
--- src/org/daisy/dotify/common/collection/ImmutableMap.java
+++ src/org/daisy/dotify/common/collection/ImmutableMap.java
@@ -2,10 +2,25 @@ package org.daisy.dotify.common.collection;
 
 import java.util.Map;
 
+/**
+ * An immutable map, i.e. a map of which the content is guaranteed not to change.
+ *
+ * All the "destructive" methods ({@link Map#put}, {@link Map#remove}, {@link Map#clear}, etc.) in
+ * this class will throw an UnsupportedOperationException.
+ *
+ * Instances of this class that are derived from each other are implemented via a common persistent
+ * data structure, i.e. a data structure that preserves previous versions of itself.
+ */
 public final class ImmutableMap<K,V> extends PersistentMap.View<K,V> implements Map<K,V> {
 
+	@SuppressWarnings("rawtypes")
+	private static final ImmutableMap EMPTY = new ImmutableMap<>(new PersistentMap<>());
+
+	/**
+	 * Get an empty instance of ImmutableList.
+	 */
 	public static <K,V> ImmutableMap<K,V> empty() {
-		return new ImmutableMap<K,V>(new PersistentMap<K,V>());
+		return (ImmutableMap<K,V>)EMPTY;
 	}
 
 	private ImmutableMap(PersistentMap<K,V> map) {
@@ -20,10 +35,17 @@ public final class ImmutableMap<K,V> extends PersistentMap.View<K,V> implements 
 		super(from, true);
 	}
 
+	/**
+	 * Get a ImmutableMap.Builder with the same initial contents as this map, in other words, a
+	 * mutable version of this map.
+	 */
 	public Builder<K,V> builder() {
 		return new Builder<K,V>(this);
 	}
 
+	/**
+	 * A builder of an ImmutableMap. Does not support removing elements.
+	 */
 	// not extending ImmutableMap so that Builder can not be accidentally passed when immutable object is expected
 	public final static class Builder<K,V> extends PersistentMap.View<K,V> implements Map<K,V> {
 
@@ -31,6 +53,9 @@ public final class ImmutableMap<K,V> extends PersistentMap.View<K,V> implements 
 			super(from, false);
 		}
 
+		/**
+		 * Build an immutable version of this map.
+		 */
 		// FIXME: set readonly to true (permanently) and cast instead of creating new object?
 		// -> one object creation less
 		// -> Builder needs to have a clone method then
@@ -89,6 +114,10 @@ public final class ImmutableMap<K,V> extends PersistentMap.View<K,V> implements 
 		readonly = true;
 	}
 
+	/**
+	 * Non-destructive version of {@link Map#put}. Instead of returning the previous value, this
+	 * method will return a new Map object with the given value associated with the given key.
+	 */
 	public static <K,V> ImmutableMap<K,V> put(ImmutableMap<K,V> map, K key, V value) {
 		// return map.builder().put(key, value).build();
 		return new ImmutableMap<K,V>(map, key, value);
